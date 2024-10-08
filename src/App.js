1c3d84aae51b6f6cc84de50199545de4221a3df4
sara <sarah_khoshaghebat@yahoo.com>
2024-10-08 16:28:51 +0330

    Redesign

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {Nav} from './components/nav';
+import { Shop } from './pages/shop/shop';
+import { Cart } from './pages/cart/cart';
 
 function App() {
   return (
     <div className="App">
-      <h1>heloo</h1>
+      <Router>
+        <Nav/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/cart' element={<Cart/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,5 @@
+export const Shop =() => {
+    return(
+        <h1>,,,,,</h1>
+    )
+}
