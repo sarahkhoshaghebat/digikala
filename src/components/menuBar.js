b9219ec6cc96bc64aad588aca198022c00430da0
sara <sarah_khoshaghebat@yahoo.com>
2024-10-16 19:06:39 +0330

    Redesign menu bar and add svg

--- src/components/menuBar.js
+++ src/components/menuBar.js
@@ -1,4 +1,4 @@
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faBasketShopping,faFire, faTachographDigital, faTag, faLocationDot, faPercent } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MenuBar = () => {
@@ -6,32 +6,57 @@ const MenuBar = () => {
     <div className="container-fluid mt-3">
       <div className="row">
         <div className="col">
-          <a href="#">لطفا شهر خود را انتخاب کنید</a>
+          <a href="#">لطفا شهر خود را انتخاب کنید
+            <span>
+            <FontAwesomeIcon icon={faLocationDot} size="sm" />
+            </span>
+          </a>
         </div>
         <div className="col">
-          <a href="#">در دیجی‌کالا بفروشید!</a>
+          <a href="#">!در دیجی‌کالا بفروشید</a>
         </div>
         <div className="col">
           <a href="#">سوالی دارید؟</a>
         </div>
         <div className="col">
-          <a href="#">تخفیف‌ها و پیشنهادها</a>
+          <a href="#">تخفیف‌ها و پیشنهادها
+            <span>
+                <FontAwesomeIcon icon={faTag} size="sm" />
+            </span>
+          </a>
         </div>
         <div className="col">
-          <a href="#">پرفروش‌ترین‌ها</a>
+          <a href="#">پرفروش‌ترین‌ها
+            <span>
+            <FontAwesomeIcon icon={faFire} size="sm" />
+            </span>
+          </a>
         </div>
         <div className="col">
-          <a href="#">کارت هدیه</a>
+          <a href="#">کارت هدیه
+            <span>
+            <FontAwesomeIcon icon={faTachographDigital} size="sm" />
+            </span>
+          </a>
         </div>
         <div className="col">
-          <a href="#">سوپرمارکت</a>
+          <a href="#">سوپرمارکت
+            <span>
+            <FontAwesomeIcon icon={faBasketShopping} size="sm" />
+            </span>
+          </a>
         </div>
         <div className="col">
-          <a href="#">شگفت‌انگیزها</a>
+          <a href="#">شگفت‌انگیزها
+            <sapan>
+            <FontAwesomeIcon icon={faPercent} size="sm" />
+            </sapan>
+          </a>
         </div>
         <div className="col">
           <a href="#">
-            دسته‌بندی کالاها
+            <span>دسته‌بندی کالاها
+            </span>
             <span>
               <FontAwesomeIcon icon={faBars} size="sm" />
             </span>
