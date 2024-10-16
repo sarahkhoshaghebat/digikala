0864b05f515469aecb7e51f0a1f607a1b7cba686
sara <sarah_khoshaghebat@yahoo.com>
2024-10-16 12:48:38 +0330

    Redesign  menu bar

--- src/components/menuBar.js
+++ src/components/menuBar.js
@@ -1,9 +1,45 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const MenuBar = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="container-fluid mt-3">
+      <div className="row">
+        <div className="col">
+          <a href="#">لطفا شهر خود را انتخاب کنید</a>
         </div>
-    );
+        <div className="col">
+          <a href="#">در دیجی‌کالا بفروشید!</a>
+        </div>
+        <div className="col">
+          <a href="#">سوالی دارید؟</a>
+        </div>
+        <div className="col">
+          <a href="#">تخفیف‌ها و پیشنهادها</a>
+        </div>
+        <div className="col">
+          <a href="#">پرفروش‌ترین‌ها</a>
+        </div>
+        <div className="col">
+          <a href="#">کارت هدیه</a>
+        </div>
+        <div className="col">
+          <a href="#">سوپرمارکت</a>
+        </div>
+        <div className="col">
+          <a href="#">شگفت‌انگیزها</a>
+        </div>
+        <div className="col">
+          <a href="#">
+            دسته‌بندی کالاها
+            <span>
+              <FontAwesomeIcon icon={faBars} size="sm" />
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MenuBar;
